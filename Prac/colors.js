8f56856a5bfef2f18ce6cc38ef3e19b7a5f6b64d
HaeunKwonDev <kwonhaeun27@gmail.com>
2025-02-12 17:01:22 +0900

    파일쪼개기

---
+++ Prac/colors.js
@@ -0,0 +1,40 @@
+
+function dayNightHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        Body.setBackColor('#1f1f1f');
+        Body.setColor('#cccccc')
+        self.value = 'day';
+
+        Links.setColor('#92add6');
+
+    } else {
+        Body.setBackColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+
+        Links.setColor('#2667cb');
+
+    }
+}
+
+var Body = {
+    setColor:function(pColor){
+        document.querySelector('body').style.color = pColor;
+    },
+    setBackColor:function(pColor){
+        document.querySelector('body').style.backgroundColor = pColor;
+    }
+
+}
+
+var Links = {
+    setColor:function(pColor){
+        var alist = document.querySelectorAll('a'); //야간모드에 모든 a 태그 색상 바꾸기
+        var i = 0;
+        while(i<alist.length){
+            alist[i].style.color= pColor;
+            i=i+1;
+        }
+    },
+}
